3cc38b020711b2b42541f5f64c49c7cc95135f8e
robroskie <lroblesky@gmail.com>
2021-06-23 12:45:47 -0700

    letter_combinations_phone complete

--- java-code/src/letter_combinations_phone_number_17.java
+++ java-code/src/letter_combinations_phone_number_17.java
@@ -1,8 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class letter_combinations_phone_number_17 {
 
 	public static void main(String[] args) {
-
+		System.out.println(letterComb("23"));
+		System.out.println(letterComb(""));
+		System.out.println(letterComb("3"));
 	}
 
+    public static List<String> letterComb(String digits) {
+    	List<String> l = new ArrayList<>();
+    	
+    	if(digits.length() == 0)
+    		return new ArrayList<>();;
+    	
+    	letterComb(digits, "", 0, l);
+    	
+    	return l;
+    }
+
+    public static void letterComb(String digits, String str, int lvl, List<String> l) {
+    	String[] letters = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+    	if(lvl < digits.length()) {
+    		String curr_letters = letters[digits.charAt(lvl) - '0' - 2];
+	    	for(int i = 0; i < curr_letters.length(); i++) {
+	    		letterComb(digits, str + curr_letters.charAt(i), lvl + 1, l);
+	    	}
+    	}
+
+    	if(lvl == digits.length()) 
+    		l.add(str);
+    }
 }
