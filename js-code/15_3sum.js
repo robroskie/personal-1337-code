6f87cf81e9ec431262e653cfe643d4c874226ed9
robroskie <robroskie@gmail.com>
2022-05-19 18:56:35 -0700

    all issues occurred due to incorrect implementation of sort function

--- js-code/15_3sum.js
+++ js-code/15_3sum.js
@@ -4,74 +4,58 @@
  */
  var threeSum = function(nums) {
     let toReturn = [];
-    nums.sort();
+    // sort the array to be parsed
+    nums.sort((a, b) => a - b);
 
     for(let i = 0; i < nums.length; i++){
-        console.log('hi out');
+
         // prevent duplicates from being found and added to return list
         if(i > 0){
-            console.log(`nums[i]=${nums[i]}`);
             while(nums[i] == nums[i - 1] && i < nums.length){
                 i++;
-                console.log('i++');
             }
         }
 
-
+        // initalize left and right pointers
         let l = i + 1;
         let r = nums.length - 1;
         
-
-
+        // while left pointer is left of the right
         while(l < r){
+            // calculate the sum
             let sum = nums[i] + nums[l] + nums[r];
-            console.log(`i=${i} current=${nums[i]} nums[l]=${nums[l]} nums[r]=${nums[r]} sum=${sum}   ||||||| l=${l} r=${r}`);
+            
+            // if sum equal to 0
+            // push the corresponding three values
+
             if(sum == 0){
-                console.log('pushing above value');
-    
                 toReturn.push([nums[i], nums[l], nums[r]]);
 
+                // move the left pointer until a new value is encountered(since we only want to use each combination once)
                 let previous = l;
                 l++;
                 while(nums[l] == nums[previous]){
-                    console.log('shifting left pointer after duplicate');
                     l++;
                 }
-             
-                if(l > nums.length)
-                    break;
-   
-
+                // reset right pointer to the end of the array so that new combinations can be considered with the updated left pointer
+                r = nums.length - 1;
             }
+            // if sum is greater than the target value, move the right pointer 1 to the left
             else if(sum > 0){
-                console.log('else if');
                 r -= 1;
             }
+            // by elimination the sum has to be less than the target value, move the left pointer 1 to the right
             else{
-                console.log('else');
                 l += 1;
             }
-
+            if(l >= nums.length)
+                break;
         }
     }
     return toReturn;
 };
 
-// console.log(threeSum([-2,0,1,1,2]));
-
-// console.log(threeSum([-1,0,1,2,-1,-4]));
-
-
-// console.log(threeSum([-2,0,1,1,2]));
-
-// console.log(threeSum([0,0,0,0]));
-
-// console.log(threeSum([-1,0,1,2,-1,-4]));
-
-console.log(threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4]));
-
-// [[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]]
 
 
 
-let difference = arrA.filter(x => !arrB.includes(x));
+console.log([-1,0,1,2,-1,-4,-2,-3,3,0,4].sort((a, b) => a - b));
