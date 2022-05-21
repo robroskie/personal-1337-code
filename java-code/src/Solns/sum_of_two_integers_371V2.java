7230bc4ee36c54583caac3019a5b1be108b73632
robroskie <robroskie@gmail.com>
2022-05-21 16:50:11 -0700

    Create sum_of_two_integers_371V2.java

---
+++ java-code/src/Solns/sum_of_two_integers_371V2.java
@@ -0,0 +1,26 @@
+package Solns;
+
+public class sum_of_two_integers_371V2 {
+	public static void main(String[] args) {
+		System.out.println(3 ^ 2);
+		System.out.println(3 & 2);
+		System.out.println((3 & 2) << 1);
+		int a = 12;
+		System.out.println("a is: " + Integer.toBinaryString(a));
+		int b = 25;
+		System.out.println("b is: " + Integer.toBinaryString(b));
+		
+
+		int result;
+
+		do {
+			result = a & b;
+			a = a ^ b;
+			b = result << 1;
+		}
+		while(b != 0);
+
+		System.out.println(a);
+		
+	}
+}
