1163818213e4d8cb089f677029390e6bd28803e4
robroskie <robroskie@gmail.com>
2022-06-13 14:16:42 -0700

    updates

---
+++ java-code/src/Solns/binary_tree_level_order_traversal_102.java
@@ -0,0 +1,10 @@
+package Solns;
+
+public class binary_tree_level_order_traversal_102 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java-code/src/Solns/invert_binary_tree_226.java
@@ -0,0 +1,22 @@
+package Solns;
+
+public class invert_binary_tree_226 {
+	class Solution {
+	    public TreeNode invertTree(TreeNode root) {
+//	    	Base case occurs when traverse to a null element
+	        if(root == null)
+	            return root;
+	        
+//	        Swap the left and right child nodes
+	        TreeNode left_temp = root.left;
+	        root.left = root.right;
+	        root.right = left_temp;
+	        
+//	        Recursively perform the algorithm on the left and right children
+	        invertTree(root.left);
+	        invertTree(root.right);
+	        
+	        return root;
+	    }
+	}
+}
