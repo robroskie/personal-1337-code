7b66cda339671cd16aaff72cd1f037f390e9101a
robroskie <lroblesky@gmail.com>
2022-09-07 05:39:45 -0700

    20 reattempted

---
+++ java-code/src/Stack/Valid_Parenthesses_20.java
@@ -0,0 +1,27 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Valid_Parenthesses_20 {
+    public boolean isValid(String s) {
+    	if(s.length() % 2 != 0)
+    		return false;
+    	
+    	Stack<Character> st = new Stack<>();
+    	
+    	for(int i = 0; i < s.length(); i++) {
+    		char current_bracket = s.charAt(i);
+    		if(current_bracket == '{') 
+    			st.push('}');
+    		else if(current_bracket == '(')
+    			st.push(')');
+    		else if(current_bracket == '[')
+    			st.push(']');		
+    		else if(st.isEmpty() || st.pop() != current_bracket)))
+				return false;
+    	}
+    }
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ java-code/src/Stack/generate_parentheses_22.java
@@ -0,0 +1,13 @@
+package Stack;
+
+import java.util.List;
+
+public class generate_parentheses_22 {
+    public List<String> generateParenthesis(int n) {
+        
+    }
+    
+	public static void main(String[] args) {
+		
+	}
+}
